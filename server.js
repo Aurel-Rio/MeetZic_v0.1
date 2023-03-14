1e2f022b79e06f4128c2fe0e8e4e91e71945423a
debian-riozacki <debian-riozacki@riozacki-ms-7d18.home>
2023-03-15 00:43:18 +0100

    MeetZic_v0.1

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 import express from 'express';
 import ejs from 'ejs';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-
 const PORT = 4000;
 
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.listen(PORT, () => {
-    console.log('Le server a bien démarré sur le port:' + PORT);
-})
+  console.log(`Le server a bien démarré sur le port: ${PORT}`);
+});
 
 app.get('/', (req, res) => {
-    const user = null; // Remplacez par vos données utilisateur
-    res.render('index', { user });
-  });
+  const user = null; // Remplacez par vos données utilisateur
+  res.render('index', { user });
+});
