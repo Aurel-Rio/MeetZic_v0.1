c7a8f65c4bc89fe3576fe390986422a3cb66fdb5
debian-riozacki <debian-riozacki@riozacki-ms-7d18.home>
2023-03-15 00:57:22 +0100

    MeetZic_v0.1

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = 4000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/public/css'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use((req, res, next) => {
+    console.log(`Erreur 404 : ${req.url}`);
+    res.status(404).send("Désolé, la page que vous recherchez est introuvable");
+  });
+  
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Oops! Une erreur est survenue');
+  });
+  
+  
 app.listen(PORT, () => {
   console.log(`Le server a bien démarré sur le port: ${PORT}`);
 });
